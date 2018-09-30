af10920e2a84f9ba1d245906af46718eadc27872
Suat Selen (Ben ) <mehmetselen@mehmet-imac.local>
2018-09-30 16:31:22 +0300

    Initial Commit

---
+++ Levoyu Yakalamaca/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Levoyu Yakalamaca
+//
+//  Created by Suat Selen on 30.09.2018.
+//  Copyright © 2018 mehmet selen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
